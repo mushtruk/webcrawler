b589802364d26d36764fee320d0db1800653d232
Ihor Mushtruk <kurthsum@gmail.com>
2023-11-11 17:57:31 +0200

    queue tests

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -1,35 +1,89 @@
-package queue_test
+package queue
 
 import (
+	"strconv"
 	"testing"
-
-	"github.com/mushtruk/webcrawler/queue"
 )
 
-type mockItem struct {
-	value string
+type IntKey int
+
+func (ik IntKey) Key() string {
+	return strconv.Itoa(int(ik)) // Convert int to string
 }
 
-func (m mockItem) Key() string {
-	return m.value
+// TestQueueAdd checks if items are correctly added to the queue.
+func TestQueueAdd(t *testing.T) {
+	q := NewQueue[IntKey]()
+	q.Add(1)
+	if q.IsEmpty() || q.Size() != 1 {
+		t.Fatalf("Queue should have 1 item after adding, got %d", q.Size())
+	}
+
+	q.Add(2)
+	if q.Size() != 2 {
+		t.Fatalf("Queue should have 2 items after adding another, got %d", q.Size())
+	}
 }
 
-func TestQueueAddAndVisited(t *testing.T) {
-	q := queue.NewQueue[mockItem]()
-	item1 := mockItem{"item1"}
+// TestQueueNext checks if Next correctly returns items in FIFO order.
+func TestQueueNext(t *testing.T) {
+	q := NewQueue[IntKey]()
+	q.Add(1)
+	q.Add(2)
 
-	q.Add(item1)
-	if q.Size() != 1 {
-		t.Errorf("Queue size should be 1, got %d", q.Size())
+	first, ok := q.Next()
+	if !ok || first != 1 {
+		t.Fatalf("Expected first Next to return 1, got %d", first)
 	}
 
-	if !q.IsVisited(item1) {
-		t.Error("Item1 should be marked as visited")
+	second, ok := q.Next()
+	if !ok || second != 2 {
+		t.Fatalf("Expected second Next to return 2, got %d", second)
 	}
 
-	// Test adding a duplicate item
-	q.Add(item1)
-	if q.Size() != 1 {
-		t.Errorf("Queue size should still be 1 after adding duplicate, got %d", q.Size())
+	if !q.IsEmpty() {
+		t.Fatal("Queue should be empty after removing all items")
+	}
+}
+
+// TestQueueIsEmpty checks if IsEmpty correctly reports the queue's status.
+func TestQueueIsEmpty(t *testing.T) {
+	q := NewQueue[IntKey]()
+	if !q.IsEmpty() {
+		t.Fatal("New queue should be empty")
+	}
+
+	q.Add(1)
+	if q.IsEmpty() {
+		t.Fatal("Queue should not be empty after adding an item")
+	}
+
+	_, _ = q.Next()
+	if !q.IsEmpty() {
+		t.Fatal("Queue should be empty after removing the item")
+	}
+}
+
+func TestQueueStress(t *testing.T) {
+	q := NewQueue[IntKey]()
+	const numItems = 10000
+
+	for i := 0; i < numItems; i++ {
+		q.Add(IntKey(i))
+	}
+
+	if q.Size() != numItems {
+		t.Errorf("Queue should have %d items, got %d", numItems, q.Size())
+	}
+
+	for i := 0; i < numItems; i++ {
+		item, ok := q.Next()
+		if !ok || item != IntKey(i) {
+			t.Fatalf("Expected %d, got %d", i, item)
+		}
+	}
+
+	if !q.IsEmpty() {
+		t.Error("Queue should be empty after removing all items")
 	}
 }
